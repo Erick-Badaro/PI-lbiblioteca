0544a8d4b4076863ddca17226fb31eb888c542df
Erick <zaterick10@gmail.com>
2025-05-06 11:01:41 -0300

    Criando o componente de login, e as rotas possiveis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ConteudoComponent } from './pages/conteudo/conteudo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ConteudoComponent,
+    },
+    {
+        path: 'conteudo',
+        component: ConteudoComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'conteudo',
+    },
+];
