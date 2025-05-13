957a0f4b1f57de5c622b5b855a910ade3ed2fa69
Gabriel A <gabriel.17.set.2005@gmail.com>
2025-05-12 21:29:37 -0300

    aplicando o svg de carrinho de compras

--- src/app/pages/visualizar-livro/visualizar-livro.component.ts
+++ src/app/pages/visualizar-livro/visualizar-livro.component.ts
@@ -12,12 +12,12 @@ import { Livro } from '../../core/types/livro';
 export class VisualizarLivroComponent {
   constructor(private livraria: LivrariaService, private router: ActivatedRoute) {}
   idLivro!: number;
-livroEscolhido!: Livro;
+  livroEscolhido!: Livro;
 
 ngOnInit(): void {
   this.idLivro = Number(this.router.snapshot.paramMap.get('id'));
   this.livraria.visualizarLivro(this.idLivro).subscribe((livro) => {
     this.livroEscolhido = livro;
-  });
+    });
 }
 }
